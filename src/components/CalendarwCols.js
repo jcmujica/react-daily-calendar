093ee115f678c6760ad9e8231bd16906e590bd0d
jc <jcmujica@gmail.com>
2020-04-12 20:26:11 -0400

    on good path

--- src/components/CalendarwCols.js
+++ src/components/CalendarwCols.js
@@ -3,9 +3,14 @@ import { Resizable, ResizableBox } from 'react-resizable';
 
 function CalendarwCols() {
   const [week, setweek] = useState({
-    mon: ['m1: 1586174400000', 'm2: 1586175300000', 'm3: 1586176200000'],
-    tue: ['t1: 1586260800000', 't2: 1586261700000', 't3: 1586262600000'],
-    wed: ['w1: 1586347200000', 'w2: 1586348100000', 'w3: 1586349000000'],
+    con: ['8:00', '8:15', '8:30', '9:00', '9:30', '10:00', '10:30', '11:00'],
+    mon: ['mon1', 'mon2', 'mon3', 'mon4', 'mon5', 'mon6', 'mon7', 'mon8'],
+    tue: ['tue1', 'tue2', 'tue3', 'tue4', 'tue5', 'tue6', 'tue7', 'tue8'],
+    wed: ['wed1', 'wed2', 'wed3', 'wed4', 'wed5', 'wed6', 'wed7', 'wed8'],
+    thu: ['thu1', 'thu2', 'thu3', 'thu4', 'thu5', 'thu6', 'thu7', 'thu8'],
+    fri: ['fri1', 'fri2', 'fri3', 'fri4', 'fri5', 'fri6', 'fri7', 'fri8'],
+    sat: ['sat1', 'sat2', 'sat3', 'sat4', 'sat5', 'sat6', 'sat7', 'sat8'],
+    sun: ['sun1', 'sun2', 'sun3', 'sun4', 'sun5', 'sun6', 'sun7', 'sun8'],
   })
   const [columns, setcolumns] = useState({
     control: ['8:00', '8:15', '8:30'],
@@ -18,23 +23,29 @@ function CalendarwCols() {
     setevents({
       ...events,
       [e.target.id]: {
-        date: e.target.id
+        date: e.target.id,
+        height: columnHeight
       }
     })
     console.log(events)
   }
 
-  const EventBox = () => {
+  const EventBox = (props) => {
+    const { date: id, height } = props.data
     return (<ResizableBox
       className='calendar-event'
       width={'100%'}
-      height={columnHeight}
+      height={height}
       axis='y'
       minConstraints={[100, 100]}
       maxConstraints={[300, 1000]}
       handleSize={[8, 8]}
-      draggableOpts={{ grid: [columnHeight, 25] }}
-      resizeHandles={['s']}>
+      draggableOpts={{ grid: [25, height] }}
+      resizeHandles={['s']}
+      onResizeStart={(e) => handleResizeStart(e)}
+      onResizeStop={(e, data) => handleResizeStop(e, data, id)}
+    >
+
       <span >Contents</span>
     </ResizableBox>)
   }
@@ -43,20 +54,41 @@ function CalendarwCols() {
     return <hr />
   }
 
+  const handleResizeStart = (e) => {
+    console.log('hola')
+  }
+  const handleResizeStop = (e, data, id) => {
+    let newHeight = data.node.parentElement.offsetHeight
+
+    setevents({
+      ...events,
+      [id]: {
+        ...[id],
+        height: newHeight
+      }
+    })
+  }
+
   const ColumnGrid = () => {
     let grid = []
     for (let i = 0; i < week.mon.length; i++) {
       for (let day in week) {
         let id = week[day][i]
         grid = [...grid,
-        <div className='column is-one-third calendar-column' key={week[day][i]} style={{ 'height': `${columnHeight}px` }} id={id} onClick={!events[week[day][i]] ? (e) => createEvent(e) : null}>
-          {events[week[day][i]] ? <EventBox /> : null}
+        <div
+          className='calendar-column'
+          key={id}
+          style={{ 'height': `${columnHeight}px` }}
+          id={id}
+          onClick={!events[id] ? (e, data) => createEvent(e, data) : null}>
+          {id}
+          {events[id] ? <EventBox data={events[id]} /> : null}
         </div>
         ]
       }
     }
     return (
-      <div className='columns is-multiline'>
+      <div className='calendar'>
         {grid}
       </div>
     )
